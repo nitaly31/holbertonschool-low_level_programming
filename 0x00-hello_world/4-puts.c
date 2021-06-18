c6a6e32628f0977233091c5d5489ec5451f44f30
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-17 22:21:18 -0700

    arreglo de errores de betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,8 +7,8 @@
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle")
 }
 {
-    return (0);
+	return (0);
 }
