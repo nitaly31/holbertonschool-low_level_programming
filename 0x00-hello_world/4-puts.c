bc61865f31cbea9c982c3bd4daf0679f5bc8848a
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-17 21:55:13 -0700

    modificación de codigo, arreglo de texto a imprimir

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-  puts(Programming is like building a multilingual puzzle)
+    puts("Programming is like building a multilingual puzzle")
 }
 {
     return (0);
