962d10c8ea7e5e96f9c673c2f3a0051bb1b49d11
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-18 22:47:14 -0700

    marcando ultimo digito

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+/**
+ * main -Positive anything is better than negative nothing
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int ult = n % 10;
+
+	if (ult > 5)
+	{
+		printf("Last digit of %d and is greater than 5\n", n);
+	}
+	else if (ult == 0)
+	{
+		printf("Last digit of %d is 0 and is 0\n", n);
+	}
+	else
+	{
+		char c[] = "Last digit of";
+		printf("%s %d is %d and is less than 6 and not 0\n", c, n, ult);
+	}
+	return (0);
+}
