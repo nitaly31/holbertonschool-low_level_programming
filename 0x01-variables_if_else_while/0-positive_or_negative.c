197329051c61a86db221a91da196ae9b12e10902
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-18 20:20:28 -0700

    correción de 0-positive_or_negative.c :)

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
+/**
+ * main -Positive anything is better than negative nothing
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
