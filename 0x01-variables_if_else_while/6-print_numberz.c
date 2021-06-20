ff59bbe7411b8a98595ea56afca685857c0e9f54
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-20 16:39:48 -0700

    conteo del 0 al 9 sin char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Positive anything is better than negative nothing
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ab;
+
+	for (ab = 48; ab <= 57; ab++)
+	{
+		putchar(ab);
+	}
+	putchar('\n');
+
+	return (0);
+}
