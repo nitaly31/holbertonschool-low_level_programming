3fc0b18006ec02b3cef8fb8d384cb21c01833a06
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-20 15:28:27 -0700

    Escribir de la a-z y A-Z con salto de línea al final

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -Positive anything is better than negative nothing
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ab;
+
+	for (ab = 97; ab <= 122; ab++)
+	{
+		putchar(ab);
+	}
+	for (ab = 65; ab <= 90; ab++)
+	{
+		putchar(ab);
+	}
+	putchar('\n');
+
+	return (0);
+}
