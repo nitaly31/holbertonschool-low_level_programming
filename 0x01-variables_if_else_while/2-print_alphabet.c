cb181fde083c917a9d80032ed4ed5237ba8b8e96
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-20 15:10:59 -0700

    escribir del a-z con salto de línea

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Positive anything is better than negative nothing
+ * Return: 0
+ */
+int main(void)
+{
+	int ab;
+
+	for (ab = 97; ab <= 122; ab++)
+	{
+		putchar(ab);
+	}
+		putchar('\n');
+
+	return (0);
+}
