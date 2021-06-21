1e67026551cf74b75e010aa031b44b5539ec140c
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-20 19:15:07 -0700

    bucle anidado (for)

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main -Positive anything is better than negative nothing
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ab;
+
+	for (ab = 48; ab <= 57; ab++)
+	{
+		for (int i = ab + 1; i <= 57; i++)
+		{
+			putchar(ab);
+			putchar(i);
+
+			if (!(ab == 56 && i == 57))
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
