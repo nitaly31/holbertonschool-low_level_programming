5cdbb8380143d2ec8b7c14c3664eeb659eb95ee6
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-20 17:34:53 -0700

    abecedario al reves

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Positive anything is better than negative nothing
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ab;
+
+	for (ab = 122; ab >= 97; ab--)
+	{
+		putchar(ab);
+	}
+	putchar('\n');
+
+	return (0);
+}
