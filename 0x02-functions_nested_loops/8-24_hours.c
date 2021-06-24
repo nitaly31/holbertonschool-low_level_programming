d42ec54ba0fafc4dece60aaa42b5f5109d0f55fe
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-23 19:33:41 -0700

    24 hora

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+ * jack_bauer - function that prints  every minute
+ *of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ * @n: num aleatorio
+ *
+ * Return: 1 and prints + if n is greater than zero, 0 and 0  if n is zero,
+ * -1 and - if n is less than zero
+*/
+
+void jack_bauer(void)
+{
+    int a;
+    int b;
+    int c;
+    int d;
+
+    for (a = 0; a <= 2; a++)
+    {
+        for (b = 0; b <= 9; b++)
+        {
+		for (c = 0; c <= 5; c++)
+		{
+			for (d = 0; d <= 9; d++)
+			{
+				if(a == 2 && b > 3)
+				{
+					break;
+				}
+				_putchar('0' + a);
+				_putchar('0' + b);
+				_putchar(58);
+				_putchar('0' + c);
+				_putchar('0' + d);
+				_putchar('\n');
+			}
+		}
+	}
+    }
+}
