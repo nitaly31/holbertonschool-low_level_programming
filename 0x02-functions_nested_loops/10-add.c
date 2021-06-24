04a8677c6d85ae4bdadeaeaa32c0db1564372872
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-23 18:18:12 -0700

    suma de numeros enteros

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ *
+ * @a: number 1.
+ * @b: number 2.
+ *
+ * Return: adds two integers and returns the result.
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
