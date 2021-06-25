53169c53a889e4f913cb8a4fc12fb5a604f7ed2e
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-25 10:55:54 -0700

    lineas como espacio

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,14 +10,14 @@
 
 void print_line(int n)
 {
-	int i=0;
+	int i = 0;
 
-	if(n > 0)
+	if (n > 0)
 	{
-		for(i = 0; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar ('_');
+			_putchar('_');
 		}
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
