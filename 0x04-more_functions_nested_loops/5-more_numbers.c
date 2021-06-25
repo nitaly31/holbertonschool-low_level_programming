9ab5b9e1a295ebb561116d138396e2c159573913
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-25 10:26:13 -0700

    imprimir del 0 al 9, 10 veces

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,18 +10,18 @@
 void more_numbers(void)
 {
 	int i;
-	char n;
+	int n;
 
-	for (i = 0; i > 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (n = 0; n <= 14; n++)
 		{
 			if (n > 9)
 			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+				_putchar('1');
 			}
-			_putchar('\n');
+			_putchar((n % 10) + '0');
 		}
+			_putchar('\n');
 	}
 }
