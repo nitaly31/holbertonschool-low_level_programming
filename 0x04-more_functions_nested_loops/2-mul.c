6c94893360755f6706a4acc8223ee9cdd5ee6f28
nitaly31 <geraldine.nitaly@gmail.com>
2021-06-25 09:32:27 -0700

    multilicación de enteros

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Indicates if a character is alphabetic.
+ *
+ * @c: Caracter ASCII imprimible.
+ *
+ * Return: Returns 1 if c is a letter, lowercase or uppercase 0 otherwise.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
