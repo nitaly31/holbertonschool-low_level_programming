fe0735b51943724dc33fb873686b14f8e391d11d
nitaly31 <geraldine.nitaly@gmail.com>
2021-07-22 15:10:15 -0700

    function that selects the correct function to perform the operation asked by the user

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -5,8 +5,28 @@
  * get_op_func - function that selects the correct function to perform
  * the operation asked by the user.
  *
- * @s: pointer
+ * @s: operation
  * Return: 0.
 */
 
 int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			break;
+		i++;
+	}
+	return (NULL);
+}
