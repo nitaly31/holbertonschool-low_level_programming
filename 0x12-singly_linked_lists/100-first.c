c1fa1ead8f8f662d848ffdf1f8642bf306ac85cb
nitaly31 <geraldine.nitaly@gmail.com>
2021-08-03 15:08:04 -0700

    function that prints a sentence before the main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
-/**
- * printf_str - function to print before the main function
- * Return: 0
-*/
-
 void printf_str(void) __attribute__((constructor));
+
+/**
+ * printf_str - prints a sentence before the main
+*/
 
 void printf_str(void)
 {
